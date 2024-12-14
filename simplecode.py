2147068087ca5d33f3ccc14a0d14e6fd2dbc353d
NISHAN-THAPA <137798141+NISHAN-THAPA@users.noreply.github.com>
2024-12-14 20:34:49 +0545

    Add files via upload

---
+++ simplecode.py
@@ -0,0 +1,7 @@
+import os
+print("hey i am learning python as well as git")
+name=input("enter your name: ")
+#print(name)
+print(os.system('free'))
+print(os.system('uptime'))
+print(os.system('df -h'))
